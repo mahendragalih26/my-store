269c027edd784515d6dfd4451b712f5179028fcd
Galih Mahendra <mahendragalih26@gmail.com>
2020-02-11 10:36:43 +0700

    first init hehe

---
+++ src/pages/main/dashboard.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+import Form from "react-jsonschema-form";
+
+class Dashboard extends Component {
+  state = {};
+  render() {
+    const schema = {
+      title: "Todo",
+      type: "object",
+      required: ["title"],
+      properties: {
+        title: { type: "string", title: "Title", default: "A new task" },
+        done: { type: "boolean", title: "Done?", default: false }
+      }
+    };
+
+    const log = type => console.log.bind(console, type);
+    return (
+      <div>
+        <Form
+          schema={schema}
+          onChange={log("changed")}
+          onSubmit={log("submitted")}
+          onError={log("errors")}
+        />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
